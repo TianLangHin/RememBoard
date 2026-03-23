bb6427238d8653b6da0f6f9535a21b846a1fc3b4
Tian Lang Hin <tianlanghin@gmail.com>
2026-03-23 22:13:18 +1100

    Added script to automate image capturing for dataset generation

---
+++ dataset/capture_image.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import os
+
+# This is the IP webcam address through which the photos of the chessboard are taken.
+ip_webcam = input('Webcam IP: ')
+
+# This will be the folder name that all the images are collected into.
+category = input('Image folder: ')
+
+if not os.path.exists(os.path.join(os.getcwd(), category)):
+    os.mkdir(category)
+
+ply_number = 1
+while True:
+    cap = cv2.VideoCapture(f'http://{ip_webcam}/video')
+    ret, frame = cap.read()
+    if not ret:
+        print('End')
+        break
+    key = input(f'Move {ply_number} image: ')
+    if key == 'quit':
+        break
+    cv2.imwrite(os.path.join(os.getcwd(), category, f'{ply_number:0>4}.png'), frame)
+    ply_number += 1
