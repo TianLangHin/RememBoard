02ace5dff5ce0c67757f424b0b2315ad4cccf410
Tian Lang Hin <tianlanghin@gmail.com>
2026-04-02 03:25:52 +1100

    Filled in React client interface with basic prototype to verify the server's functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,120 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
+import { Typography } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = createTheme({
+    colorSchemes: {
+      dark: true,
+      light: true,
+    },
+  })
+
+  const [fens, setFens] = useState<string[]>([])
+  const [images, setImages] = useState<string[]>([])
+
+  const connection = useRef<WebSocket | null>(null)
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:19941')
+    socket.addEventListener('open', _ => {
+      socket.send('client')
+    })
+    socket.addEventListener('close', _ => {
+      console.log('closed')
+    })
+    socket.addEventListener('message', event => {
+      var newImages: string[] = []
+      var newFens: string[] = []
+      const processedImages = event.data.split(',')
+      for (const data of processedImages) {
+        const d = data.split('|')
+        newImages.push(d[0])
+        newImages.push(d[1])
+      }
+      setFens(newFens)
+      setImages(newImages)
+    })
+    connection.current = socket
+    return () => connection.current?.close()
+  }, [])
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <ThemeProvider theme={theme}>
+      <h1>Client</h1>
+      <div style={{display: 'flex'}}>
+        {
+          Array.from(images.entries()).map(([index, image]) => (
+            <div style={{flex: 1}} key={index}>
+              <img src={'data:image/png;base64,' + image} width="640px" height="360px" />
+            </div>
+          ))
+        }
+      </div>
+        {
+          Array.from(fens.entries()).map(([index, fen]) => (
+            <Typography key={index}>{fen}</Typography>
+          ))
+        }
+      <div>
+      </div>
+    </ThemeProvider>
   )
 }
 
